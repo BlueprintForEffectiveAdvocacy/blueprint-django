817c66b9d867ba170a9f12668eef60da88ea21c7
Phuc <pnguyen@macalester.edu>
2017-09-23 15:30:07 -0500

    remove Pagecontent model

--- blueprint/main/models.py
+++ blueprint/main/models.py
@@ -1,20 +1,12 @@
 from django.db import models
 
-
-class PageContent(models.Model):
-    title = models.CharField(max_length=64)
+    
+class Issue(models.Model):
+    issue_name = models.CharField(max_length=64)
     summary = models.CharField(max_length=250)
     body = models.CharField(max_length=500)
     image = models.CharField(max_length=64)
     video = models.CharField(max_length=64)
-    
-class Issue(models.Model):
-    issue_name = models.CharField(max_length=64)
-    page_content = models.OneToOneField(
-        PageContent,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
     
 class Organization(models.Model):
     org_name = models.CharField(max_length=64)
