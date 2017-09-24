2547a4a0bd2be9fb47b2b592274d4ff8d327d666
jea345 <jordan.akers@icloud.com>
2017-09-24 11:18:21 -0500

    made issue columns nullable

--- blueprint/main/models.py
+++ blueprint/main/models.py
@@ -3,11 +3,11 @@ from django.db import models
     
 class Issue(models.Model):
     issue_name = models.CharField(max_length=64)
-    scope = models.CharField(max_length=64)
-    summary = models.CharField(max_length=250)
-    body = models.CharField(max_length=500)
-    image = models.CharField(max_length=64)
-    video = models.CharField(max_length=64)
+    scope = models.CharField(max_length=64, null=True, blank=True)
+    summary = models.CharField(max_length=250, null=True, blank=True)
+    body = models.CharField(max_length=500, null=True, blank=True)
+    image = models.CharField(max_length=64, null=True, blank=True)
+    video = models.CharField(max_length=64, null=True, blank=True)
     
 class Organization(models.Model):
     org_name = models.CharField(max_length=64)
