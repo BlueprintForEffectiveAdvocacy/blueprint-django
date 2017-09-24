67cbdf41586cf388d68ee2546c763425b0e96fc0
jea345 <jordan.akers@icloud.com>
2017-09-24 12:15:38 -0500

    added middleware

--- blueprint/blueprint/settings.py
+++ blueprint/blueprint/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'blueprint.middleware.MyMiddleware',
 ]
 
 ROOT_URLCONF = 'blueprint.urls'
